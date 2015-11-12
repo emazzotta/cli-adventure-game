f0116225ea8a43ed2cf5df2b1c515adf2f1d6af2
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2015-11-12 15:05:03 +0100

    Added items to rooms

--- src/main/java/com/mazzotta/kuster/pointandclick/adventure/game/elements/items/Potion.java
+++ src/main/java/com/mazzotta/kuster/pointandclick/adventure/game/elements/items/Potion.java
@@ -3,13 +3,17 @@ package com.mazzotta.kuster.pointandclick.adventure.game.elements.items;
 public class Potion {
 
     private final String name;
+    private final int healthRestorePoints;
 
-    public Potion(String name) {
+    public Potion(String name, int healthRestorePoints) {
         this.name = name;
+        this.healthRestorePoints = healthRestorePoints;
     }
 
     public String getName() {
         return name;
     }
-
+    public int getHealthRestorePoints() {
+        return healthRestorePoints;
+    }
 }
--- src/main/java/com/mazzotta/kuster/pointandclick/adventure/level/Initialiser.java
+++ src/main/java/com/mazzotta/kuster/pointandclick/adventure/level/Initialiser.java
@@ -1,6 +1,9 @@
 package com.mazzotta.kuster.pointandclick.adventure.level;
 
 import com.mazzotta.kuster.pointandclick.adventure.game.elements.Room;
+import com.mazzotta.kuster.pointandclick.adventure.game.elements.items.Item;
+import com.mazzotta.kuster.pointandclick.adventure.game.elements.items.Potion;
+import com.mazzotta.kuster.pointandclick.adventure.game.elements.items.Weapon;
 
 import java.util.ArrayList;
 
@@ -22,6 +25,28 @@ public class Initialiser {
 
 
     public void initialise() {
+        addRooms();
+        addItemsToRooms();
+    }
+
+    private void addItemsToRooms() {
+        Weapon shortSword = new Weapon("Short Sword", 100);
+        Weapon longSword = new Weapon("Long Sword", 200);
+        Weapon masterSword = new Weapon("Master Sword", 500);
+
+        Potion healthPotion50 = new Potion("Health +50", 50);
+        Potion healthPotion100 = new Potion("Health +100", 100);
+        Potion healthPotion150 = new Potion("Health +150", 150);
+
+        room.get(0).addItem((Item)shortSword);
+        room.get(2).addItem((Item)healthPotion50);
+        room.get(2).addItem((Item)healthPotion100);
+        room.get(6).addItem((Item)longSword);
+        room.get(7).addItem((Item)healthPotion150);
+        room.get(8).addItem((Item)masterSword);
+    }
+
+    private void addRooms() {
         room.add(new Room("Room 1"));
         room.add(new Room("Room 2"));
         room.add(new Room("Room 3"));
