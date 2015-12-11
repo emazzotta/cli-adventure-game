a80198aa3eeb5de29b2a7efb7807efbacfa138d4
Leandro Kuster <leandro@kusterfamily.com>
2015-12-11 20:35:54 +0100

    convert input to command test added

---
+++ src/test/java/command/CommandTest.java
@@ -0,0 +1,26 @@
+package command;
+
+import com.mazzotta.kuster.pointandclick.adventure.commands.Command;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandTest {
+
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void convertToCommandTest() {
+        Assert.assertEquals(Command.HELP, Command.convertToCommand("Help"));
+    }
+
+    @Test
+    public void convertInvalidCommandTest() {
+        Assert.assertEquals(Command.NONE, Command.convertToCommand("InvalidCommandStatement"));
+    }
+
+
+}
