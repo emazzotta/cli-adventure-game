29f34318c8be2741709657698a0272c4b3f13f38
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2015-12-17 12:43:01 +0100

    refactoring

--- src/main/java/com/mazzotta/kuster/pointandclick/adventure/exceptions/UserDiedException.java
+++ src/main/java/com/mazzotta/kuster/pointandclick/adventure/exceptions/UserDiedException.java
@@ -1,8 +1,7 @@
 package com.mazzotta.kuster.pointandclick.adventure.exceptions;
 
 public class UserDiedException extends Exception {
-    public UserDiedException(String message)
-    {
+    public UserDiedException(String message) {
         super(message);
     }
 }
