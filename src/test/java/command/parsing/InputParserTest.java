1d44da39cfd1369a8c7fe5122b1df6031f0f254b
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2015-12-17 12:39:48 +0100

    Added input validator test

--- src/test/java/command/parsing/InputParserTest.java
+++ src/test/java/command/parsing/InputParserTest.java
@@ -1,4 +1,4 @@
-package command;
+package command.parsing;
 
 import com.mazzotta.kuster.pointandclick.adventure.commands.CommandAction;
 import com.mazzotta.kuster.pointandclick.adventure.commands.parsing.InputParser;
@@ -9,10 +9,18 @@ import static junit.framework.TestCase.assertEquals;
 public class InputParserTest {
 
     @Test
-    public void testThatUserInputCanBeCorrectlyParsedToCommandActionObject() {
+    public void testThatAValidUserInputCanBeCorrectlyParsedToCommandActionObject() {
         String command = "collect items";
         CommandAction commandAction = new InputParser().getCommandActionFrom(command);
         assertEquals(commandAction.getCommand().toString(), "COLLECT");
         assertEquals(commandAction.getActionType().toString(), "ITEMS");
+    }
+
+    @Test
+    public void testThatAnInvalidUserInputIsConvertedToNone() {
+        String command = "asfds";
+        CommandAction commandAction = new InputParser().getCommandActionFrom(command);
+        assertEquals(commandAction.getCommand().toString(), "NONE");
+        assertEquals(commandAction.getActionType().toString(), "NONE");
     }
 }
