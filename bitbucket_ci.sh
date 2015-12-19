9a1dd39082884e4413720021e1d3e38b399a1c9f
Emanuele Mazzotta <mazzotta.emanuele@gmail.com>
2015-12-19 18:14:54 +0100

    removed non needed bitbucket api stuff since shippable can do it on its own

--- bitbucket_ci.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -i \
---user it15_ta_win:yVOnYOVO0YMB8gkeTsnMxIxPiDMz3UfB \
--H "Content-Type: application/json" \
--H "Accept: application/json" \
--X POST -d '{"key": "'$JOB_ID'", "state": "'"$1"'", "name": "Shippable Unit Tests", "url": "'$BUILD_URL'", "description": "Build: '$1'"}' \
-https://api.bitbucket.org/2.0/repositories/it15_ta_win/point-and-click-adventure/commit/$COMMIT/statuses/build
